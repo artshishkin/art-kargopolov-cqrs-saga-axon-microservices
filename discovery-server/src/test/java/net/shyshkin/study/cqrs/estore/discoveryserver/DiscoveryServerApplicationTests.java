4029790bacc0d5a88ca0c9f88d39d30ceddec4ce
Art <d.art.shishkin@gmail.com>
2021-05-16 17:03:57 +0300

    23. Creating new project (#2 Section 4: Eureka Discovery)

---
+++ discovery-server/src/test/java/net/shyshkin/study/cqrs/estore/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.cqrs.estore.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
