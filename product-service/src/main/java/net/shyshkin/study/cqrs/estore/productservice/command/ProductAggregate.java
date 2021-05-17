cd9e9a6f846c18495fc5e683e2f6df5e7d5b1654
Art <d.art.shishkin@gmail.com>
2021-05-17 15:08:47 +0300

    50. Validate the CreateProductCommand [skip ci] (#5)

--- product-service/src/main/java/net/shyshkin/study/cqrs/estore/productservice/command/ProductAggregate.java
+++ product-service/src/main/java/net/shyshkin/study/cqrs/estore/productservice/command/ProductAggregate.java
@@ -3,6 +3,8 @@ package net.shyshkin.study.cqrs.estore.productservice.command;
 import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.spring.stereotype.Aggregate;
 
+import java.math.BigDecimal;
+
 @Aggregate
 public class ProductAggregate {
 
@@ -11,7 +13,14 @@ public class ProductAggregate {
 
     @CommandHandler
     public ProductAggregate(CreateProductCommand createProductCommand) {
-        // TODO: 17.05.2021 Validate Create Product Command
+
+        if (createProductCommand.getPrice().compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("Price can not be less or equal to zero");
+        }
+
+        if (createProductCommand.getTitle() == null || createProductCommand.getTitle().isEmpty()) {
+            throw new IllegalArgumentException("Title can not be empty");
+        }
     }
 
 
