efd4500a63307103a69c881c06c868ebf61bb57e
Art <d.art.shishkin@gmail.com>
2021-05-17 19:32:50 +0300

    55. Trying how it works - validation failed (closes #5)

--- product-service/src/test/java/net/shyshkin/study/cqrs/estore/productservice/rest/ProductControllerManualTest.java
+++ product-service/src/test/java/net/shyshkin/study/cqrs/estore/productservice/rest/ProductControllerManualTest.java
@@ -3,8 +3,11 @@ package net.shyshkin.study.cqrs.estore.productservice.rest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
@@ -21,7 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Slf4j
 @SpringBootTest
 @DisplayName("Start axon-server docker-compose file first")
-//@Disabled("Only for manual testing")
+@Disabled("Only for manual testing")
 class ProductControllerManualTest {
 
     MockMvc mockMvc;
@@ -56,6 +59,32 @@ class ProductControllerManualTest {
                 //then
                 .andExpect(status().isOk())
                 .andExpect(content().string(startsWith("Http POST: ")))
+        ;
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            ",125.0,Http POST: Title can not be empty",
+            "iPhone 3,-125.0,Http POST: Price can not be less or equal to zero"
+    })
+    void createProduct_validationFailed(String title, String price, String expectedErrorMessage) throws Exception {
+
+        //given
+        CreateProductRestModel createProductRestModel = CreateProductRestModel.builder()
+                .title(title)
+                .price(new BigDecimal(price))
+                .quantity(2)
+                .build();
+        String jsonPayload = objectMapper.writeValueAsString(createProductRestModel);
+
+        //when
+        mockMvc.perform(post("/products")
+                .contentType(APPLICATION_JSON)
+                .content(jsonPayload))
+
+                //then
+                .andExpect(status().isOk())
+                .andExpect(content().string(expectedErrorMessage))
         ;
     }
 }
