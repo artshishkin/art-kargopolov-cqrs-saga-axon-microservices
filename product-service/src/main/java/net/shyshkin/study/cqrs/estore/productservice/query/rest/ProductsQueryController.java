8c7bb67e6bbe0b1ed8f9548fb0d8cf7e16789656
Art <d.art.shishkin@gmail.com>
2021-05-18 16:10:38 +0300

    67. Creating Rest Controller (#9 Section 9: CQRS. Querying Data)

---
+++ product-service/src/main/java/net/shyshkin/study/cqrs/estore/productservice/query/rest/ProductsQueryController.java
@@ -0,0 +1,10 @@
+package net.shyshkin.study.cqrs.estore.productservice.query.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductsQueryController {
+
+}
