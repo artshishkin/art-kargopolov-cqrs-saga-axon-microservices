470fa7febe571756b7591094e831ce07ef135bc6
Art <d.art.shishkin@gmail.com>
2021-05-18 16:54:29 +0300

    70. Querying the QueryGateway (#9)

---
+++ product-service/src/main/java/net/shyshkin/study/cqrs/estore/productservice/query/FindProductsQuery.java
@@ -0,0 +1,4 @@
+package net.shyshkin.study.cqrs.estore.productservice.query;
+
+public class FindProductsQuery {
+}
--- product-service/src/main/java/net/shyshkin/study/cqrs/estore/productservice/query/rest/ProductsQueryController.java
+++ product-service/src/main/java/net/shyshkin/study/cqrs/estore/productservice/query/rest/ProductsQueryController.java
@@ -1,18 +1,30 @@
 package net.shyshkin.study.cqrs.estore.productservice.query.rest;
 
+import lombok.RequiredArgsConstructor;
+import net.shyshkin.study.cqrs.estore.productservice.query.FindProductsQuery;
+import org.axonframework.messaging.responsetypes.ResponseTypes;
+import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/products")
+@RequiredArgsConstructor
 public class ProductsQueryController {
+
+    private final QueryGateway queryGateway;
 
     @GetMapping
     public List<ProductRestModel> getProducts() {
-        throw new RuntimeException("Not implemented yet");
+
+        FindProductsQuery findProductsQuery = new FindProductsQuery();
+
+        CompletableFuture<List<ProductRestModel>> query = queryGateway.query(findProductsQuery, ResponseTypes.multipleInstancesOf(ProductRestModel.class));
+        return query.join();
     }
 
 }
