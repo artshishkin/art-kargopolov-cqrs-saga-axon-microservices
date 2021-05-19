415fc371f970c34d2ae4ad31b71d12e576399a07
Art <d.art.shishkin@gmail.com>
2021-05-19 22:28:31 +0300

    83. Register Message Dispatch Interceptor (#11)

--- product-service/src/main/java/net/shyshkin/study/cqrs/estore/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/net/shyshkin/study/cqrs/estore/productservice/ProductServiceApplication.java
@@ -1,9 +1,12 @@
 package net.shyshkin.study.cqrs.estore.productservice;
 
+import net.shyshkin.study.cqrs.estore.productservice.command.interceptors.CreateProductCommandInterceptor;
+import org.axonframework.commandhandling.CommandBus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,6 +14,11 @@ public class ProductServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
+    }
+
+    @Autowired
+    public void registerCreateProductCommandInterceptor(ApplicationContext context, CommandBus commandBus) {
+        commandBus.registerDispatchInterceptor(context.getBean(CreateProductCommandInterceptor.class));
     }
 
 }
