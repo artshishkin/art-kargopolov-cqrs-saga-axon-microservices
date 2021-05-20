6f40e0389abe1844e41cf9dbe2e904b3b655bcd1
Art <d.art.shishkin@gmail.com>
2021-05-20 16:28:22 +0300

    91. Trying how the Set base validation works (closes #12)

--- product-service/src/test/java/net/shyshkin/study/cqrs/estore/productservice/command/interceptors/CreateProductCommandInterceptorTest.java
+++ product-service/src/test/java/net/shyshkin/study/cqrs/estore/productservice/command/interceptors/CreateProductCommandInterceptorTest.java
@@ -1,12 +1,16 @@
 package net.shyshkin.study.cqrs.estore.productservice.command.interceptors;
 
+import com.github.javafaker.Faker;
 import lombok.extern.slf4j.Slf4j;
 import net.shyshkin.study.cqrs.estore.productservice.command.rest.CreateProductRestModel;
+import net.shyshkin.study.cqrs.estore.productservice.core.data.ProductLookupEntity;
+import net.shyshkin.study.cqrs.estore.productservice.core.data.ProductLookupRepository;
 import net.shyshkin.study.cqrs.estore.productservice.testcontainers.AxonServerContainer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.TestPropertySource;
@@ -14,6 +18,7 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -32,6 +37,9 @@ class CreateProductCommandInterceptorTest {
 
     @Autowired
     TestRestTemplate restTemplate;
+
+    @Autowired
+    ProductLookupRepository lookupRepository;
 
     @Container
     public static AxonServerContainer axonServer = AxonServerContainer.getInstance();
@@ -61,5 +69,76 @@ class CreateProductCommandInterceptorTest {
         log.debug("Response body: {}", body);
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(body).contains(errorMessage);
+    }
+
+    @Test
+    void createProduct_alreadyExists() {
+
+        //given
+        String title = getTitleOfExistingProduct();
+        String price = "666.00";
+        String errorMessage = "already exists";
+        String errorPattern = CreateProductCommandInterceptor.PRODUCT_EXISTS_PATTERN;
+
+        CreateProductRestModel createProductRestModel = CreateProductRestModel.builder()
+                .title(title)
+                .price(new BigDecimal(price))
+                .quantity(2)
+                .build();
+
+        //when
+        ResponseEntity<String> responseEntity = restTemplate
+                .postForEntity("/products",
+                        createProductRestModel,
+                        String.class);
+
+        //then
+        String body = responseEntity.getBody();
+        log.debug("Response body: {}", body);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(body).startsWith("Http POST: ");
+        assertThat(body).contains(errorMessage);
+
+    }
+
+    private String getTitleOfExistingProduct() {
+
+        return lookupRepository
+                .findAll(PageRequest.of(0, 1))
+                .get()
+                .findAny()
+                .map(ProductLookupEntity::getTitle)
+                .orElseGet(this::postNewCreateProductRestModel);
+    }
+
+    private String postNewCreateProductRestModel() {
+
+        //given
+        String title = Faker.instance().commerce().productName();
+        String price = "666.00";
+        String errorMessage = "Already exists";
+
+        CreateProductRestModel createProductRestModel = CreateProductRestModel.builder()
+                .title(title)
+                .price(new BigDecimal(price))
+                .quantity(2)
+                .build();
+
+        //when
+        ResponseEntity<String> responseEntity = restTemplate
+                .postForEntity("/products",
+                        createProductRestModel,
+                        String.class);
+
+        //then
+        String body = responseEntity.getBody();
+        log.debug("Response body: {}", body);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(body).startsWith("Http POST: ");
+        String productId = body.replace("Http POST: ", "");
+        UUID uuid = UUID.fromString(productId);
+        assertThat(uuid).isNotNull();
+
+        return title;
     }
 }
