c838d59efed1d5e8695b260c4593366463879ba4
Art <d.art.shishkin@gmail.com>
2021-05-23 08:46:57 +0300

    102. Refactored CreateOrderCommand to have default Status as CREATED (#14)

--- order-service/src/main/java/net/shyshkin/study/cqrs/estore/orderservice/command/CreateOrderCommand.java
+++ order-service/src/main/java/net/shyshkin/study/cqrs/estore/orderservice/command/CreateOrderCommand.java
@@ -1,5 +1,6 @@
 package net.shyshkin.study.cqrs.estore.orderservice.command;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import net.shyshkin.study.cqrs.estore.orderservice.core.OrderStatus;
@@ -8,17 +9,20 @@ import java.util.UUID;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class CreateOrderCommand {
 
     @Builder.Default
     private final UUID orderId = UUID.randomUUID();
 
     @Builder.Default
-    private final UUID userId =UUID.fromString("27b95829-4f3f-4ddf-8983-151ba010e35b");
+    private final UUID userId = UUID.fromString("27b95829-4f3f-4ddf-8983-151ba010e35b");
 
     private final UUID productId;
     private final int quantity;
     private final UUID addressId;
-    private final OrderStatus orderStatus;
+
+    @Builder.Default
+    private OrderStatus orderStatus = OrderStatus.CREATED;
 
 }
