51c3ec8eca08157e72771150e81c0b292a79599e
Art <d.art.shishkin@gmail.com>
2021-05-24 13:11:53 +0300

    108. @SagaEventHandler method for the OrderCreatedEvent (#15)

--- order-service/src/main/java/net/shyshkin/study/cqrs/estore/orderservice/saga/OrderSaga.java
+++ order-service/src/main/java/net/shyshkin/study/cqrs/estore/orderservice/saga/OrderSaga.java
@@ -1,6 +1,9 @@
 package net.shyshkin.study.cqrs.estore.orderservice.saga;
 
+import net.shyshkin.study.cqrs.estore.orderservice.core.events.OrderCreatedEvent;
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.modelling.saga.SagaEventHandler;
+import org.axonframework.modelling.saga.StartSaga;
 import org.axonframework.spring.stereotype.Saga;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -9,4 +12,10 @@ public class OrderSaga {
 
     @Autowired
     private transient CommandGateway commandGateway;
+
+    @StartSaga
+    @SagaEventHandler(associationProperty = "orderId")
+    public void handle(OrderCreatedEvent orderCreatedEvent){
+
+    }
 }
