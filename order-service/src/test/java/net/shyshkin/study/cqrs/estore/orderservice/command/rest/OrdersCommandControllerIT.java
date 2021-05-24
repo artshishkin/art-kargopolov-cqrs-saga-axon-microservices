983f8b3a82826630ef738b3c92929434a1fff4b6
Art <d.art.shishkin@gmail.com>
2021-05-24 21:54:53 +0300

    Using DockerComposeContainer for integration testing

---
+++ order-service/src/test/java/net/shyshkin/study/cqrs/estore/orderservice/command/rest/OrdersCommandControllerIT.java
@@ -0,0 +1,129 @@
+package net.shyshkin.study.cqrs.estore.orderservice.command.rest;
+
+import com.github.javafaker.Faker;
+import lombok.extern.slf4j.Slf4j;
+import net.shyshkin.study.cqrs.estore.orderservice.command.CreateOrderCommand;
+import net.shyshkin.study.cqrs.estore.orderservice.core.OrderStatus;
+import net.shyshkin.study.cqrs.estore.orderservice.core.data.OrdersRepository;
+import net.shyshkin.study.cqrs.estore.orderservice.testcontainers.TestComposeContainer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.*;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.web.client.RestTemplate;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.net.URI;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
+@Slf4j
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = {
+        "eureka.client.enabled=false",
+        "spring.datasource.url=jdbc:h2:mem:testdb",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "axon.axonserver.servers=${AXON_SERVERS}"
+})
+@Testcontainers
+class OrdersCommandControllerIT {
+
+    @Container
+    public static TestComposeContainer testComposeContainer = TestComposeContainer.getInstance();
+
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @Autowired
+    RestTemplateBuilder restTemplateBuilder;
+
+    RestTemplate restTemplate;
+
+    @Autowired
+    OrdersRepository ordersRepository;
+
+    @BeforeEach
+    void setUp() {
+        String rootUri = "http://" + testComposeContainer.getProductServiceHost() + ":" + testComposeContainer.getProductServicePort();
+        log.debug("Root URI: {}", rootUri);
+        restTemplate = restTemplateBuilder.rootUri(rootUri).build();
+    }
+
+    @Test
+    void createOrder_correct() {
+        //given
+        UUID productId = createProduct();
+        CreateOrderRestModel createOrderRestModel = CreateOrderRestModel.builder()
+                .productId(productId)
+                .addressId(UUID.fromString("afbb5881-a872-4d13-993c-faeb8350eea5"))
+                .quantity(1)
+                .build();
+
+        //when
+        ResponseEntity<CreateOrderCommand> responseEntity = testRestTemplate
+                .postForEntity("/orders", createOrderRestModel, CreateOrderCommand.class);
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        URI location = responseEntity.getHeaders().getLocation();
+        assertThat(location)
+                .isNotNull();
+
+        log.debug("Response entity: {}", responseEntity);
+
+        String locationString = location.toString();
+        String orderIdString = locationString.substring(locationString.lastIndexOf("/") + 1);
+        UUID orderId = UUID.fromString(orderIdString);
+
+        await()
+                .timeout(1, TimeUnit.SECONDS)
+                .untilAsserted(() ->
+                        assertThat(ordersRepository.findByOrderId(orderId))
+                                .hasValueSatisfying(orderEntity ->
+                                        assertThat(orderEntity)
+                                                .hasNoNullFieldsOrProperties()
+                                                .satisfies(entity -> log.debug("Entity: {}", entity))
+                                                .hasFieldOrPropertyWithValue("orderId", orderId)
+                                                .hasFieldOrPropertyWithValue("productId", createOrderRestModel.getProductId())
+                                                .hasFieldOrPropertyWithValue("addressId", createOrderRestModel.getAddressId())
+                                                .hasFieldOrPropertyWithValue("quantity", createOrderRestModel.getQuantity())
+                                                .hasFieldOrPropertyWithValue("orderStatus", OrderStatus.CREATED)
+                                )
+                );
+
+        await()
+                .timeout(100, TimeUnit.MILLISECONDS);
+        log.debug("View in logs (current or another instance of `order-service`): `OrderCreatedEvent is handled` followed by `ProductReservedEvent is handled`");
+    }
+
+    private UUID createProduct() {
+        String productName = Faker.instance().commerce().productName();
+        String createProductRestModelJson =
+                "{" +
+                        "    \"title\":\"" + productName + "\"," +
+                        "    \"price\":12.00," +
+                        "    \"quantity\":5" +
+                        "}";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+        HttpEntity<String> requestEntity = new RequestEntity<>(createProductRestModelJson, headers, HttpMethod.POST, null, null);
+        ResponseEntity<String> responseEntity = restTemplate.exchange(
+                "/products",
+                HttpMethod.POST,
+                requestEntity,
+                String.class);
+        String response = responseEntity.getBody();
+        response = response.replace("Http POST: ", "");
+        return UUID.fromString(response);
+    }
+}
---
+++ order-service/src/test/java/net/shyshkin/study/cqrs/estore/orderservice/testcontainers/TestComposeContainer.java
@@ -0,0 +1,54 @@
+package net.shyshkin.study.cqrs.estore.orderservice.testcontainers;
+
+import lombok.Getter;
+import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+
+import java.io.File;
+
+@Getter
+public class TestComposeContainer extends DockerComposeContainer<TestComposeContainer> {
+
+    private static final String COMPOSE_FILE_PATH = "src/test/resources/compose-test.yml";
+    private static TestComposeContainer container;
+
+    private String productServiceHost;
+    private Integer productServicePort;
+
+    public TestComposeContainer() {
+        super(new File(COMPOSE_FILE_PATH));
+    }
+
+    public static TestComposeContainer getInstance() {
+        if (container == null) {
+            container = new TestComposeContainer()
+                    .withExposedService("axon-server_1", 8124,
+                            Wait.forLogMessage(".*Started AxonServer in.*\\n", 1))
+                    .withExposedService("product-service_1", 8080,
+                            Wait.forHealthcheck()
+                    );
+
+        }
+        return container;
+    }
+
+    @Override
+    public void start() {
+
+        super.start();
+
+        String axonHost = container.getServiceHost("axon-server_1", 8124);
+        Integer axonPort = container.getServicePort("axon-server_1", 8124);
+        String servers = axonHost + ":" + axonPort;
+        System.setProperty("AXON_SERVERS", servers);
+
+        productServiceHost = container.getServiceHost("product-service_1", 8080);
+        productServicePort = container.getServicePort("product-service_1", 8080);
+
+    }
+
+    @Override
+    public void stop() {
+        //do nothing, JVM handles shut down
+    }
+}
