81dcfbd5d5e8fb58e3df71520ddf948ea58159a5
Art <d.art.shishkin@gmail.com>
2021-05-24 11:29:00 +0300

    102.8/9. Run and make it work, Verify results (closes #14)

---
+++ order-service/src/test/java/net/shyshkin/study/cqrs/estore/orderservice/command/rest/OrdersCommandControllerManualTest.java
@@ -0,0 +1,74 @@
+package net.shyshkin.study.cqrs.estore.orderservice.command.rest;
+
+import lombok.extern.slf4j.Slf4j;
+import net.shyshkin.study.cqrs.estore.orderservice.command.CreateOrderCommand;
+import net.shyshkin.study.cqrs.estore.orderservice.core.OrderStatus;
+import net.shyshkin.study.cqrs.estore.orderservice.core.data.OrdersRepository;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.net.URI;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
+@Slf4j
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Disabled("Only for manual testing")
+class OrdersCommandControllerManualTest {
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Autowired
+    OrdersRepository ordersRepository;
+
+    @Test
+    void createOrder_correct() {
+        //given
+        CreateOrderRestModel createOrderRestModel = CreateOrderRestModel.builder()
+                .productId(UUID.fromString("f241af45-4854-43f4-95bc-ab54da338a29"))
+                .addressId(UUID.fromString("afbb5881-a872-4d13-993c-faeb8350eea5"))
+                .quantity(1)
+                .build();
+
+        //when
+        ResponseEntity<CreateOrderCommand> responseEntity = restTemplate
+                .postForEntity("/orders", createOrderRestModel, CreateOrderCommand.class);
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        URI location = responseEntity.getHeaders().getLocation();
+        assertThat(location)
+                .isNotNull();
+
+        log.debug("Response entity: {}", responseEntity);
+
+        String locationString = location.toString();
+        String orderIdString = locationString.substring(locationString.lastIndexOf("/") + 1);
+        UUID orderId = UUID.fromString(orderIdString);
+
+        await()
+                .timeout(1, TimeUnit.SECONDS)
+                .untilAsserted(() ->
+                        assertThat(ordersRepository.findByOrderId(orderId))
+                                .hasValueSatisfying(orderEntity ->
+                                        assertThat(orderEntity)
+                                                .hasNoNullFieldsOrProperties()
+                                                .satisfies(entity -> log.debug("Entity: {}", entity))
+                                                .hasFieldOrPropertyWithValue("orderId", orderId)
+                                                .hasFieldOrPropertyWithValue("productId", createOrderRestModel.getProductId())
+                                                .hasFieldOrPropertyWithValue("addressId", createOrderRestModel.getAddressId())
+                                                .hasFieldOrPropertyWithValue("quantity", createOrderRestModel.getQuantity())
+                                                .hasFieldOrPropertyWithValue("orderStatus", OrderStatus.CREATED)
+                                )
+                );
+    }
+}
