b94764a1046fb82150c5fffcb7097335f0ff9a3c
Art <d.art.shishkin@gmail.com>
2021-05-24 21:55:40 +0300

    disable Api Gateway common test

--- api-gateway/src/test/java/net/shyshkin/study/cqrs/estore/apigateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/net/shyshkin/study/cqrs/estore/apigateway/ApiGatewayApplicationTests.java
@@ -1,9 +1,11 @@
 package net.shyshkin.study.cqrs.estore.apigateway;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled("No need to test")
 class ApiGatewayApplicationTests {
 
     @Test
