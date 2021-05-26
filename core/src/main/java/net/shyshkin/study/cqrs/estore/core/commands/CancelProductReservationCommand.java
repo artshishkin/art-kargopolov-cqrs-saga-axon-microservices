daf0fa6bfdb7bc831ce0764b0f84c835490ef949
Art <d.art.shishkin@gmail.com>
2021-05-26 14:10:13 +0300

    135. Creating the CancelProductReservation command (#21 Section 20: SAGA. Compensating Transactions)

---
+++ core/src/main/java/net/shyshkin/study/cqrs/estore/core/commands/CancelProductReservationCommand.java
@@ -0,0 +1,20 @@
+package net.shyshkin.study.cqrs.estore.core.commands;
+
+import lombok.Builder;
+import lombok.Data;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+import java.util.UUID;
+
+@Data
+@Builder
+public class CancelProductReservationCommand {
+
+    @TargetAggregateIdentifier
+    private final UUID productId;
+    private final int quantity;
+    private final UUID orderId;
+    private final UUID userId;
+
+    private final String reason;
+}
