441da8719505c26ef59ddea8fd537abf0c39adf8
Art <d.art.shishkin@gmail.com>
2021-05-26 12:02:46 +0300

    131. Handle the OrderApprovedEvent and update Orders database (#20)

--- order-service/src/main/java/net/shyshkin/study/cqrs/estore/orderservice/query/OrderEventsHandler.java
+++ order-service/src/main/java/net/shyshkin/study/cqrs/estore/orderservice/query/OrderEventsHandler.java
@@ -4,11 +4,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.shyshkin.study.cqrs.estore.orderservice.core.data.OrderEntity;
 import net.shyshkin.study.cqrs.estore.orderservice.core.data.OrdersRepository;
+import net.shyshkin.study.cqrs.estore.orderservice.core.events.OrderApprovedEvent;
 import net.shyshkin.study.cqrs.estore.orderservice.core.events.OrderCreatedEvent;
 import net.shyshkin.study.cqrs.estore.orderservice.core.mapper.OrderMapper;
 import org.axonframework.config.ProcessingGroup;
 import org.axonframework.eventhandling.EventHandler;
 import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.UUID;
 
 @Slf4j
 @Component
@@ -22,6 +26,16 @@ public class OrderEventsHandler {
     @EventHandler
     public void on(OrderCreatedEvent orderCreatedEvent) {
         OrderEntity orderEntity = mapper.toEntity(orderCreatedEvent);
+        repository.save(orderEntity);
+    }
+
+    @EventHandler
+    public void on(OrderApprovedEvent orderApprovedEvent) {
+        UUID orderId = orderApprovedEvent.getOrderId();
+        OrderEntity orderEntity = repository
+                .findByOrderId(orderId)
+                .orElseThrow(() -> new EntityNotFoundException("Order with id `" + orderId + "` not found"));
+        orderEntity.setOrderStatus(orderApprovedEvent.getOrderStatus());
         repository.save(orderEntity);
     }
 
