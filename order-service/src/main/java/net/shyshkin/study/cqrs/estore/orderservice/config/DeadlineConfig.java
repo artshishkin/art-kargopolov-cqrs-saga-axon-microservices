0041566a93570875ee2cbeb2a8cf461c745d9647
Art <d.art.shishkin@gmail.com>
2021-05-28 15:01:54 +0300

    145. Creating a DeadlineManager object (#22 Section 21: Deadlines)

---
+++ order-service/src/main/java/net/shyshkin/study/cqrs/estore/orderservice/config/DeadlineConfig.java
@@ -0,0 +1,22 @@
+package net.shyshkin.study.cqrs.estore.orderservice.config;
+
+import org.axonframework.config.Configuration;
+import org.axonframework.config.ConfigurationScopeAwareProvider;
+import org.axonframework.deadline.DeadlineManager;
+import org.axonframework.deadline.SimpleDeadlineManager;
+import org.axonframework.spring.messaging.unitofwork.SpringTransactionManager;
+import org.springframework.context.annotation.Bean;
+
+@org.springframework.context.annotation.Configuration
+public class DeadlineConfig {
+
+    @Bean
+    public DeadlineManager deadlineManager(Configuration configuration,
+                                           SpringTransactionManager transactionManager) {
+        return SimpleDeadlineManager.builder()
+                .scopeAwareProvider(new ConfigurationScopeAwareProvider(configuration))
+                .transactionManager(transactionManager)
+                .build();
+    }
+
+}
