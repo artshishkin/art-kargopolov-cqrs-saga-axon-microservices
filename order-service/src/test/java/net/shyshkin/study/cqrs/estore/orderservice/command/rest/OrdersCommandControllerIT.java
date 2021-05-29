81f98109d6d3f6fd27d1173776e79b5759fd8cdf
Art <d.art.shishkin@gmail.com>
2021-05-30 01:21:57 +0300

    155. Trying how it works (#23)

--- order-service/src/test/java/net/shyshkin/study/cqrs/estore/orderservice/command/rest/OrdersCommandControllerIT.java
+++ order-service/src/test/java/net/shyshkin/study/cqrs/estore/orderservice/command/rest/OrdersCommandControllerIT.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import net.shyshkin.study.cqrs.estore.orderservice.command.CreateOrderCommand;
 import net.shyshkin.study.cqrs.estore.orderservice.core.OrderStatus;
 import net.shyshkin.study.cqrs.estore.orderservice.core.data.OrdersRepository;
+import net.shyshkin.study.cqrs.estore.orderservice.core.model.OrderSummary;
 import net.shyshkin.study.cqrs.estore.orderservice.testcontainers.TestComposeContainer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -63,7 +64,7 @@ class OrdersCommandControllerIT {
     @Test
     void createOrder_correct() throws InterruptedException {
         //given
-        UUID productId = createProduct();
+        UUID productId = createProduct(5);
         CreateOrderRestModel createOrderRestModel = CreateOrderRestModel.builder()
                 .productId(productId)
                 .addressId(UUID.fromString("afbb5881-a872-4d13-993c-faeb8350eea5"))
@@ -109,7 +110,7 @@ class OrdersCommandControllerIT {
                 " followed by `ProductReservedEvent is handled` " +
                 " followed by `Successfully fetched payment details` " +
                 " followed by `PaymentProcessedEvent is handled` " +
-                " followed by `OrderSaga if competed for order with Id` "
+                " followed by `OrderSaga is completed for order with Id` "
         );
 
         // Must have Status APPROVED
@@ -130,13 +131,127 @@ class OrdersCommandControllerIT {
                 );
     }
 
-    private UUID createProduct() {
+    @Test
+    void createOrderSubscribing_approved() {
+        //given
+        UUID productId = createProduct(1);
+        CreateOrderRestModel createOrderRestModel = CreateOrderRestModel.builder()
+                .productId(productId)
+                .addressId(UUID.fromString("afbb5881-a872-4d13-993c-faeb8350eea5"))
+                .quantity(1)
+                .build();
+
+        //when
+        ResponseEntity<OrderSummary> responseEntity = testRestTemplate
+                .postForEntity("/orders/with_subscription", createOrderRestModel, OrderSummary.class);
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        URI location = responseEntity.getHeaders().getLocation();
+        assertThat(location).isNotNull();
+
+        OrderSummary orderSummary = responseEntity.getBody();
+        assertThat(orderSummary.getMessage()).isEmpty();
+        assertThat(orderSummary.getOrderStatus()).isEqualTo(OrderStatus.APPROVED);
+
+        log.debug("Response entity: {}", responseEntity);
+
+        String locationString = location.toString();
+        String orderIdString = locationString.substring(locationString.lastIndexOf("/") + 1);
+        UUID orderId = UUID.fromString(orderIdString);
+
+        assertThat(orderSummary.getOrderId()).isEqualTo(orderId);
+
+
+        log.debug("View in logs (current or another instance of `order-service`): `OrderCreatedEvent is handled`" +
+                " followed by `ProductReservedEvent is handled` " +
+                " followed by `Successfully fetched payment details` " +
+                " followed by `PaymentProcessedEvent is handled` " +
+                " followed by `OrderSaga is completed for order with Id` "
+        );
+
+        // Must have Status APPROVED
+        await()
+                .timeout(1, TimeUnit.SECONDS)
+                .untilAsserted(() ->
+                        assertThat(ordersRepository.findByOrderId(orderId))
+                                .hasValueSatisfying(orderEntity ->
+                                        assertThat(orderEntity)
+                                                .hasNoNullFieldsOrProperties()
+                                                .satisfies(entity -> log.debug("Entity: {}", entity))
+                                                .hasFieldOrPropertyWithValue("orderId", orderId)
+                                                .hasFieldOrPropertyWithValue("productId", createOrderRestModel.getProductId())
+                                                .hasFieldOrPropertyWithValue("addressId", createOrderRestModel.getAddressId())
+                                                .hasFieldOrPropertyWithValue("quantity", createOrderRestModel.getQuantity())
+                                                .hasFieldOrPropertyWithValue("orderStatus", OrderStatus.APPROVED)
+                                )
+                );
+    }
+
+    @Test
+    void createOrderSubscribing_notEnough_rejected() {
+        //given
+        UUID productId = createProduct(1);
+        CreateOrderRestModel createOrderRestModel = CreateOrderRestModel.builder()
+                .productId(productId)
+                .addressId(UUID.fromString("afbb5881-a872-4d13-993c-faeb8350eea5"))
+                .quantity(2)
+                .build();
+
+        //when
+        ResponseEntity<OrderSummary> responseEntity = testRestTemplate
+                .postForEntity("/orders/with_subscription", createOrderRestModel, OrderSummary.class);
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        URI location = responseEntity.getHeaders().getLocation();
+        assertThat(location).isNotNull();
+
+        OrderSummary orderSummary = responseEntity.getBody();
+        assertThat(orderSummary.getMessage()).isEqualTo("Insufficient number of items in stock");
+        assertThat(orderSummary.getOrderStatus()).isEqualTo(OrderStatus.REJECTED);
+
+        log.debug("Response entity: {}", responseEntity);
+
+        String locationString = location.toString();
+        String orderIdString = locationString.substring(locationString.lastIndexOf("/") + 1);
+        UUID orderId = UUID.fromString(orderIdString);
+
+        assertThat(orderSummary.getOrderId()).isEqualTo(orderId);
+
+        log.debug("View in logs (current or another instance of `order-service`): `OrderCreatedEvent is handled`" +
+                " followed by `ProductReservedEvent is handled` " +
+                " followed by `There was an Exception: org.axonframework.commandhandling.CommandExecutionException:Insufficient number of items in stock` " +
+                " followed by `Order has been rejected` " +
+                " followed by `OrderRejectedEvent is handled` " +
+                " followed by `OrderSaga is rejected for order with Id` "
+        );
+
+        // Must have Status REJECTED
+        await()
+                .timeout(1, TimeUnit.SECONDS)
+                .untilAsserted(() ->
+                        assertThat(ordersRepository.findByOrderId(orderId))
+                                .hasValueSatisfying(orderEntity ->
+                                        assertThat(orderEntity)
+                                                .hasNoNullFieldsOrProperties()
+                                                .satisfies(entity -> log.debug("Entity: {}", entity))
+                                                .hasFieldOrPropertyWithValue("orderId", orderId)
+                                                .hasFieldOrPropertyWithValue("productId", createOrderRestModel.getProductId())
+                                                .hasFieldOrPropertyWithValue("addressId", createOrderRestModel.getAddressId())
+                                                .hasFieldOrPropertyWithValue("quantity", createOrderRestModel.getQuantity())
+                                                .hasFieldOrPropertyWithValue("orderStatus", OrderStatus.REJECTED)
+                                )
+                );
+    }
+
+    private UUID createProduct(int count) {
         String productName = Faker.instance().commerce().productName();
         String createProductRestModelJson =
                 "{" +
                         "    \"title\":\"" + productName + "\"," +
                         "    \"price\":12.00," +
-                        "    \"quantity\":5" +
+                        "    \"quantity\":" + count +
                         "}";
 
         HttpHeaders headers = new HttpHeaders();
