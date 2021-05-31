5c8211359159f7d5d21329009463a662146d671d
Art <d.art.shishkin@gmail.com>
2021-05-31 17:27:46 +0300

    162. Starting the Replay of Events (#28)

--- product-service/src/main/java/net/shyshkin/study/cqrs/estore/productservice/command/rest/EventsReplayController.java
+++ product-service/src/main/java/net/shyshkin/study/cqrs/estore/productservice/command/rest/EventsReplayController.java
@@ -2,18 +2,37 @@ package net.shyshkin.study.cqrs.estore.productservice.command.rest;
 
 import lombok.RequiredArgsConstructor;
 import org.axonframework.config.EventProcessingConfiguration;
+import org.axonframework.eventhandling.TrackingEventProcessor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/management")
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/management")
 @RequiredArgsConstructor
 public class EventsReplayController {
 
     private final EventProcessingConfiguration eventProcessingConfiguration;
 
     @PostMapping("/eventProcessor/{processorName}/reset")
-    public void resetEvents(@PathVariable String processorName) {
+    public ResponseEntity<String> resetEvents(@PathVariable String processorName) {
+        Optional<TrackingEventProcessor> eventProcessorOptional = eventProcessingConfiguration.eventProcessor(processorName, TrackingEventProcessor.class);
+        if (eventProcessorOptional.isPresent()) {
 
+            TrackingEventProcessor eventProcessor = eventProcessorOptional.get();
+            eventProcessor.shutDown();
+            eventProcessor.resetTokens();
+            eventProcessor.start();
+
+            return ResponseEntity.ok(String.format("The event processor with the name `%s` has been reset", processorName));
+        } else {
+            return ResponseEntity
+                    .badRequest()
+                    .body(String.format("The event processor with the name `%s` is not a tracking event processor", processorName));
+        }
     }
 }
